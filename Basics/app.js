cd45aa7a37d7b71e1b9471f03eca1d06aedb1653
Archan26 <archanparmar99@gmail.com>
2022-05-04 09:26:09 +0530

    Object andArray

--- Basics/app.js
+++ Basics/app.js
@@ -1,15 +1,24 @@
 var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var buttonElement = document.querySelector("button");
+var numResults = [];
+var textResults = [];
 function add(num1, num2) {
     return num1 + num2;
+}
+function printResult(resultObj) {
+    console.log(resultObj.val);
 }
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResults.push(result);
     var stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
-    console.log(add(true, false));
+    textResults.push(stringResult);
+    // console.log(result)
+    // console.log(stringResult)
+    // console.log(add(true, false))
+    printResult({ val: result, timeStamp: new Date() });
+    console.log(numResults, textResults);
 });
--- Basics/app.ts
+++ Basics/app.ts
@@ -2,16 +2,27 @@ const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
+const numResults: number[] = []
+const textResults: string[] = []
+
 function add(num1: any, num2: any) {
     return num1 + num2;
+}
+
+function printResult(resultObj: { val: number; timeStamp: Date }) {
+    console.log(resultObj.val)
 }
 
 buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2)
+    numResults.push(result)
     const stringResult = add(num1, num2)
-    console.log(result)
-    console.log(stringResult)
-    console.log(add(true, false))
+    textResults.push(stringResult)
+    // console.log(result)
+    // console.log(stringResult)
+    // console.log(add(true, false))
+    printResult({ val: result as number, timeStamp: new Date() })
+    console.log(numResults, textResults)
 })
